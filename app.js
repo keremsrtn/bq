55aecc0e3ee50c0597f01f1c57de12a038beed45
Kerem <kerem.ozkilic@gmail.com>
2019-03-25 12:33:38 +0300

    app engine update

--- app.js
+++ app.js
@@ -4,43 +4,6 @@ const express = require('express');
 
 const app = express();
 
-// Imports the Google Cloud client library
-const {BigQuery} = require('@google-cloud/bigquery');
-
-// Creates a client
-const bigquery = new BigQuery();
-
-// The SQL query to run
-const sqlQuery = `SELECT
-  CONCAT(
-    'https://stackoverflow.com/questions/',
-    CAST(id as STRING)) as url,
-  view_count
-  FROM \`bigquery-public-data.stackoverflow.posts_questions\`
-  WHERE tags like '%google-bigquery%'
-  ORDER BY view_count DESC
-  LIMIT 10`;
-
-const options = {
-  query: sqlQuery,
-  // Location must match that of the dataset(s) referenced in the query.
-  location: 'US',
-};
-
-const startQuery = async function() {
-  // Runs the query
-  const [rows] = await bigquery.query(options);
-
-  console.log('Query Results:');
-  rows.forEach(row => {
-    const url = row['url'];
-    const viewCount = row['view_count'];
-    console.log(`url: ${url}, ${viewCount} views`);
-  });
-}
-
-startQuery();
-
 app.get('/', (req, res) => {
   //res.status(200).send('Hello, world! ');
   res.send("works");
